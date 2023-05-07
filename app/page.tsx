615d196a466ec6fb250d0f681d4639a000084188
S <s.free.engineer@gmail.com>
2023-05-08 08:57:34 +0900

    fix: next.config.js にてビルドエラー無視設定を追加

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,14 @@ export default async function Page() {
       <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
         <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
           <section className="flex justify-center gap-6 sm:gap-10 md:gap-16">
-            <div className="xl:w-8/12 flex flex-col justify-center sm:text-center lg:text-left lg:py-12 xl:py-24">
+            <div className="flex justify-center">
+              <h1 className="text-black-800 text-4xl sm:text-5xl md:text-6xl font-bold mb-8 md:mb-12">
+                トークルーレットーー！
+              </h1>
+            </div>
+          </section>
+          <section className="flex justify-center gap-6 sm:gap-10 md:gap-16">
+            <div className="flex justify-center">
               <p className="text-pink-500 md:text-lg xl:text-xl font-semibold mb-4 md:mb-6">
                 雑談のネタが思いつからないからと言ってWebサービスに頼ろうとする雑なあなたへ…
               </p>
